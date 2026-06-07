cbbcfe353da0d1ccdf67ea8b5f1d0df6ca59382e
Mike Brouillette <mbrouil3@proton.me>
2026-06-06 22:43:54 -0400

    Update backup_ts-server.sh
    - Changed $TARG_DIR to $TARGETDIR
    - Added doco regarding "auto grab working directory" 
    - Fixed find bug for .txt files

--- backup_ts-server.sh
+++ backup_ts-server.sh
@@ -9,10 +9,13 @@
 
 RUNDATE=$(date +%Y-%m-%d)
 RUNTIME=$(date +%Y-%m-%d_%H%M)
-SOURCEDIR=/app/ts6server-beta/amd64                              ## TO-DO: modify to non-hardcoded dir
-TARG_DIR=/archive/level1/backups/ts6server-beta
-COUNT=$(ls ${TARG_DIR} | grep -c $(date +%Y-%m-%d'\.'))
-VAR_NUM=$(ls ${TARG_DIR} | grep -cv '.gz\|.txt')
+SOURCEDIR=/app/ts6server-beta/amd64                       ## scratched TO-DO: modify to non-hardcoded dir
+#SOURCEDIR="$( cd "$( dirname "$0" )" && pwd )"           ## Automatically grabs current working directory (no more hard coded values!)
+                                                          ##  DEV NOTE: After testing, I realized this methodology is not intuitive.
+                                                          ##            I elected to leave both values in for function and documentation purposes.
+TARGETDIR=/archive/level1/backups/ts6server-beta
+COUNT=$(ls ${TARGETDIR} | grep -c $(date +%Y-%m-%d'\.'))
+VAR_NUM=$(ls ${TARGETDIR} | grep -cv '.gz\|.txt')
 FILE_CNT=$((COUNT+1))
 FILE_NAME=ts6server-beta_${RUNDATE}.${FILE_CNT}.tar
 JOB_LOG=${SOURCEDIR}/logs/ts6server-beta_backup_${RUNTIME}.log
@@ -22,8 +25,8 @@ fn_debug() {
 ##-- Used for testing
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | ===================================== "   >  ${JOB_LOG}
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Run Date   = " "${RUNDATE}"               >> ${JOB_LOG}
-echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Targ. Dir  = " "${TARG_DIR}"              >> ${JOB_LOG}
-echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Targ File  = " "${TARG_DIR}/${FILE_NAME}" >> ${JOB_LOG}
+echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Targ. Dir  = " "${TARGETDIR}"              >> ${JOB_LOG}
+echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Targ File  = " "${TARGETDIR}/${FILE_NAME}" >> ${JOB_LOG}
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Count      = " "${COUNT}"                 >> ${JOB_LOG}
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | Var Num    = " "${VAR_NUM}"               >> ${JOB_LOG}
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | File Count = " "${FILE_CNT}"              >> ${JOB_LOG}
@@ -38,11 +41,11 @@ echo $(date +%Y-%m-%d" "%H:%M:%S)" | DEBUG | ===================================
 fn_findAndDelete() {
 ## Delete data & files older than x days
 DAYS=9
-find ${TARG_DIR} -mtime +${DAYS} -name '*.gz' -delete
-find ${TARG_DIR} -mtime +${DAYS} -name '*.gz' -delete                >> ${JOB_LOG}
+find ${TARGETDIR} -mtime +${DAYS} -name '*.gz' -delete
+find ${TARGETDIR} -mtime +${DAYS} -name '*.gz' -delete                >> ${JOB_LOG}
 
-find ${TARG_DIR} -mtime +${DAYS}-name '*.txt' -delete
-find ${TARG_DIR} -mtime +${DAYS} -name '*.txt' -delete               >> ${JOB_LOG}
+find ${TARGETDIR} -mtime +${DAYS} -name '*.txt' -delete
+find ${TARGETDIR} -mtime +${DAYS} -name '*.txt' -delete               >> ${JOB_LOG}
 
 find ${SOURCEDIR}/logs -mtime +${DAYS} -name '*.log' -delete
 find ${SOURCEDIR}/logs -mtime +${DAYS} -name '*.log' -delete         >> ${JOB_LOG}
@@ -55,10 +58,10 @@ fn_backup() {
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backing up server... please wait..." 
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backing up server... please wait..."     >> ${JOB_LOG}
 
-tar -cpf ${TARG_DIR}/"${FILE_NAME}" ${SOURCEDIR}
+tar -cpf ${TARGETDIR}/"${FILE_NAME}" ${SOURCEDIR}
 
-echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backup complete & located at: ${TARG_DIR}/${FILE_NAME}" 
-echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backup complete & located at: ${TARG_DIR}/${FILE_NAME}"  >> ${JOB_LOG}
+echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backup complete & located at: ${TARGETDIR}/${FILE_NAME}" 
+echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Backup complete & located at: ${TARGETDIR}/${FILE_NAME}"  >> ${JOB_LOG}
 }
 
 ##################
@@ -70,11 +73,11 @@ if [ "$VAR_NUM" = 0 ] ;  then
         else 
     while [ "${VAR_NUM}" -gt 0 ] 
         do
-	GZIP_FILE=$(ls -t ${TARG_DIR}| grep tar | grep -v '.tar.gz' | tail -n1 | awk '{print $1 }') 
-	echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation started for file ${GZIP_FILE}" 
-	echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation started for file ${GZIP_FILE}"   >> ${JOB_LOG}
+        GZIP_FILE=$(ls -t ${TARGETDIR}| grep tar | grep -v '.tar.gz' | tail -n1 | awk '{print $1 }') 
+        echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation started for file ${GZIP_FILE}" 
+        echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation started for file ${GZIP_FILE}"   >> ${JOB_LOG}
 ## Comment out next line when debugging/testing and not want to actually perform the pigz operation
-        pigz -9 ${TARG_DIR}/"${GZIP_FILE}"
+        pigz -9 ${TARGETDIR}/"${GZIP_FILE}"
         echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation finishd for file ${GZIP_FILE}" 
         echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | PIGZ operation finishd for file ${GZIP_FILE}" >> ${JOB_LOG}
         VAR_NUM=$((VAR_NUM-1))
@@ -89,10 +92,10 @@ fi
 
 ##################
 fn_outputDirSize () {
-ARCHIVE_UTIL=$(ls -lh ${TARG_DIR} | head -n1 | awk '{print $2}')
+ARCHIVE_UTIL=$(ls -lh ${TARGETDIR} | head -n1 | awk '{print $2}')
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Archive Size Dir written to temp file" 
 echo $(date +%Y-%m-%d" "%H:%M:%S)" | INFO | Archive Size Dir written to temp file" >> ${JOB_LOG}
-$(touch ${TARG_DIR}/${RUNTIME}_dir_size_is_${ARCHIVE_UTIL}.txt)
+$(touch ${TARGETDIR}/${RUNTIME}_dir_size_is_${ARCHIVE_UTIL}.txt)
 }
 ##################
 
